ca86273bbfeca86afc6f58917f9f560cbcb97201
mayakorablina <maykorablina@gmail.com>
2024-03-17 23:58:12 +0300

    contest 5 task 3

--- 2023_2024_2nd_sem/contest5/task3/main.cpp
+++ 2023_2024_2nd_sem/contest5/task3/main.cpp
@@ -1,38 +1,91 @@
-//
-// Created by Майя Кораблина on 08/02/24.
-//
-#include "iostream"
+#include <cmath>
 
-std::string common_suffix(const std::string& a, const std::string& b){
-    std::string ans = "";
-    std::string ans_reversed = "";
+class Complex
+{
+private:
+    double real_part;
+    double imaginary_part;
 
-    for (int i = 0; i < std::min(a.length(), b.length()); i++) {
+public:
+    Complex(double r_p, double i_p = 0) : real_part(r_p), imaginary_part(i_p) {}
 
-        if (a[a.length() - 1 - i] == b[b.length() - 1 - i]) {
-            ans += a[a.length() - 1 - i];
-        }
-        else {
-            break;
-        }
+    Complex operator+(Complex b) const
+    {
+        return Complex(real_part + b.real_part, imaginary_part + b.imaginary_part);
     }
 
-    for (int j = ans.length() - 1; j >= 0; j--) {
-        ans_reversed += ans[j];
+    Complex operator-(Complex b) const
+    {
+        return Complex(real_part - b.real_part, imaginary_part - b.imaginary_part);
     }
 
+    Complex operator*(Complex b) const
+    {
+        return Complex(real_part * b.real_part - imaginary_part * b.imaginary_part,
+                       real_part * b.imaginary_part + imaginary_part * b.real_part);
+    }
 
-    return ans_reversed;
+    Complex operator/(Complex b) const
+    {
+        double divisor = b.real_part * b.real_part + b.imaginary_part * b.imaginary_part;
+        return Complex((real_part * b.real_part + imaginary_part * b.imaginary_part) / divisor,
+                       (imaginary_part * b.real_part - real_part * b.imaginary_part) / divisor);
+    }
+
+    bool operator==(Complex b) const
+    {
+        return (real_part == b.real_part) && (imaginary_part == b.imaginary_part);
+    }
+
+    bool operator!=(Complex b) const
+    {
+        return (real_part != b.real_part) || (imaginary_part != b.imaginary_part);
+    }
+
+    Complex operator+() const
+    {
+        return Complex(real_part, imaginary_part);
+    }
+
+    Complex operator-() const
+    {
+        return Complex(-real_part, -imaginary_part);
+    }
+
+    double Re() const
+    {
+        return real_part;
+    }
+
+    double Im() const
+    {
+        return imaginary_part;
+    }
+};
+
+Complex operator+(double a, Complex b)
+{
+    return Complex(b.Re() + a, b.Im());
 }
 
-int main () {
-    std::string a;
-    std::string b;
-    std::cin >> a >> b;
-
-    std::cout << common_suffix(a, b);
-
-
-    return 0;
+Complex operator-(double a, Complex b)
+{
+    return Complex(a - b.Re(), b.Im());
 }
 
+Complex operator*(double a, Complex b)
+{
+    return Complex(a * b.Re(), a * b.Im());
+}
+
+Complex operator/(double a, Complex b)
+{
+    return Complex((a * b.Re()) / (b.Re() * b.Re() + b.Im() * b.Im()),
+                   (a * b.Im()) / (b.Re() * b.Re() + b.Im() * b.Im()));
+}
+
+double abs(Complex a)
+{
+    double sum = a.Re() * a.Re() + a.Im() * a.Im();
+    return std::sqrt(sum);
+}
