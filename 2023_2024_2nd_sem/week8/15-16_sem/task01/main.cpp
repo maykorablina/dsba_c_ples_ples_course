2ff1fc333333dbfcf4186bae772547adb62a5dc8
mayakorablina <maykorablina@gmail.com>
2024-04-01 18:47:21 +0300

    week 8 init

--- 2023_2024_2nd_sem/week8/15-16_sem/task01/main.cpp
+++ 2023_2024_2nd_sem/week8/15-16_sem/task01/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector>
+#include "string"
 /*
 You work as an analyst in a company that sells electronics. You are provided 
 with monthly sales of products for a year in the form of a vector, where each 
@@ -32,8 +33,85 @@ Month with maximum sales volume: 11
 Average monthly sales: 17000
 Sales above average: false false false true true false true true true true true true
 Number of months with sales below average: 3
-
 */
+
+int maximum(std::vector<int> sales) {
+    int m = sales[0];
+    for (int i = 1; i < sales.size(); i++) {
+        if (sales[i] > m) m = sales[i];
+    }
+    return m;
+}
+
+int pt1(std::vector<int> sales){
+    int sum = 0;
+    for (int i = 0; i < sales.size(); ++i) {
+        sum += sales[i];
+    }
+    return sum;
+}
+
+int pt2(std::vector<int> sales){
+    int m = maximum(sales);
+    int counter = 0;
+    for (int i = 0; i < sales.size(); i++) {
+        if (sales[i] == m){
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int pt21(std::vector<int> sales){
+    int m = maximum(sales);
+    int counter = 0;
+    for (int i = 0; i < sales.size(); i++) {
+        if (sales[i] < m){
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int pt3(std::vector<int> sales){
+    return (pt1(sales)) / sales.size();
+}
+
+std::vector<bool> pt4(std::vector<int> sales) {
+    int a = pt3(sales);
+    std::vector<bool> bool_data(sales.size(), false);
+    for (int i = 0; i < bool_data.size(); i++) {
+        if (sales[i] > a) {
+            bool_data[i] = true;
+        }
+    }
+    return bool_data;
+}
+
+std::string vac_to_string(std::vector<int> sales) {
+    std::vector<bool> b = pt4(sales);
+    std::string ans = "";
+    for (int i = 0; i < b.size(); i++) {
+        ans += std::to_string(b[i]);
+        ans += " ";
+    }
+    return ans;
+}
+
+Total sales volume for the year: 204000
+Month with maximum sales volume: 11
+Average monthly sales: 17000
+Sales above average: false false false true true false true true true true true true
+Number of months with sales below average: 3
+
+
+int print_data(std::vector<int> sales) {
+    std::cout << "Total sales volume for the year: " << pt1 << '\n';
+    std::cout << "Month with maximum sales volume: " << pt2 << '\n';
+    std::cout << "Average monthly sales: " << pt3 << '\n';
+    std::cout << "Month with maximum sales volume: " << vac_to_string(sales) << '\n';
+
+}
 
 int main() {
   std::vector<int> sales = {12000, 15000, 13000, 19000, 17000, 16000,
