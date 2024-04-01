02e7c00ddd1145e0586961d6ca464570504f13dc
mayakorablina <maykorablina@gmail.com>
2024-04-01 18:57:24 +0300

    week 8 task 1

--- 2023_2024_2nd_sem/week8/15-16_sem/task01/main.cpp
+++ 2023_2024_2nd_sem/week8/15-16_sem/task01/main.cpp
@@ -62,8 +62,12 @@ int pt2(std::vector<int> sales){
     return counter;
 }
 
+int pt3(std::vector<int> sales){
+    return (pt1(sales)) / sales.size();
+}
+
 int pt21(std::vector<int> sales){
-    int m = maximum(sales);
+    int m = pt3(sales);
     int counter = 0;
     for (int i = 0; i < sales.size(); i++) {
         if (sales[i] < m){
@@ -73,9 +77,7 @@ int pt21(std::vector<int> sales){
     return counter;
 }
 
-int pt3(std::vector<int> sales){
-    return (pt1(sales)) / sales.size();
-}
+
 
 std::vector<bool> pt4(std::vector<int> sales) {
     int a = pt3(sales);
@@ -89,33 +91,34 @@ std::vector<bool> pt4(std::vector<int> sales) {
 }
 
 std::string vac_to_string(std::vector<int> sales) {
+    int avg = pt3(sales);
     std::vector<bool> b = pt4(sales);
     std::string ans = "";
     for (int i = 0; i < b.size(); i++) {
-        ans += std::to_string(b[i]);
-        ans += " ";
+        if (sales[i] > avg) {
+            ans += "true";
+            ans += " ";
+        }
+        else {
+            ans += "false";
+            ans += " ";
+        }
     }
     return ans;
 }
 
-Total sales volume for the year: 204000
-Month with maximum sales volume: 11
-Average monthly sales: 17000
-Sales above average: false false false true true false true true true true true true
-Number of months with sales below average: 3
-
-
-int print_data(std::vector<int> sales) {
-    std::cout << "Total sales volume for the year: " << pt1 << '\n';
-    std::cout << "Month with maximum sales volume: " << pt2 << '\n';
-    std::cout << "Average monthly sales: " << pt3 << '\n';
-    std::cout << "Month with maximum sales volume: " << vac_to_string(sales) << '\n';
-
+void print_data(std::vector<int> sales) {
+    std::cout << "Total sales volume for the year: " << pt1(sales) << '\n';
+    std::cout << "Month with maximum sales volume: " << pt2(sales) << '\n';
+    std::cout << "Average monthly sales: " << pt3(sales) << '\n';
+    std::cout << "Sales above average: " << vac_to_string(sales) << '\n';
+    std::cout << "Number of months with sales below average: " << pt21(sales) << '\n';
 }
 
 int main() {
-  std::vector<int> sales = {12000, 15000, 13000, 19000, 17000, 16000,
+    std::vector<int> sales = {12000, 15000, 13000, 19000, 17000, 16000,
                             18000, 21000, 20000, 17000, 22000, 19000};
+    print_data(sales);
   
   return 0;
 }
