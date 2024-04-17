870ca819dbf3da8de97cedbec08752c642b0490a
mayakorablina <maykorablina@gmail.com>
2024-04-17 19:35:17 +0300

    week 8 task 4 full

--- 2023_2024_2nd_sem/week8/15-16_sem/task04/main.cpp
+++ 2023_2024_2nd_sem/week8/15-16_sem/task04/main.cpp
@@ -4,6 +4,7 @@
 #include <numeric>
 #include <vector>
 
+
 /*
 Create a program that manages a dictionary of terms and 
 their definitions. Implement functionalities to read term-definition 
@@ -56,96 +57,113 @@ void loadDictionary(const std::string& filename, std::map<std::string, std::stri
     }
 }
 
+std::string capitalize(std::string str) {
+    std::string ans = "";
+    ans += static_cast<char>(std::toupper(str[0]));
+    for (int i = 1; i < str.length(); i++) {
+        ans += static_cast<char>(std::tolower(str[i]));
+    }
+    return ans;
+}
+
+
+
 struct Dictionary{
 private:
     std::map<std::string, std::string> dictionary;
 public:
-    Dictionary(std::string path) {
+    Dictionary(const std::string& path) {
         loadDictionary(path, dictionary);
     }
+    void lookupTerm(const std::string& term) {
+        std::string a = capitalize(term);
+        auto it = dictionary.find(a);
+        if (it != dictionary.end()) {
+            std::cout << it->second << '\n';
+        } else {
+            std::cout << "\033[31m" << '"' << a << '"' << " is not in dictionary." << "\033[0m" << '\n';
+        }
+    }
+    void addTerm(std::string word) {
+        word = capitalize(word);
+        auto it = dictionary.find(word);
+        if (it != dictionary.end()) {
+            std::cout << "\033[31m" << '"' << word << '"' << " is already in the dictionary." << "\033[0m" << '\n';
+        } else {
+            std::string definition;
+            std::cout << "Enter the definition to " << word << ": ";
+            std::cin >> definition;
+            dictionary[word] = capitalize(definition);
+            std::cout << "\033[32m" << '"' << word << '"' << " was successfully added to the dictionary." << "\033[0m" << '\n';
+        }
 
-
+    }
+    void deleteTerm(const std::string term) {
+        std::string a = capitalize(term);
+        auto it = dictionary.find(a);
+        if (it != dictionary.end()) {
+            dictionary.erase(a);
+            std::cout << "\033[32m" << '"' << a << '"' << " was successfully removed from the dictionary." << "\033[0m" << '\n';
+        } else {
+            std::cout << "\033[31m" << '"' << a << '"' << " is not in dictionary." << "\033[0m1" << '\n';
+        }
+    }
+    void saveDictionary(const std::string& filename) {
+        std::ofstream out(filename);
+        if (!out) {
+            std::cout << "\033[31m" << "Can't open the file." << '\n';
+        } else{
+            for (const auto& pair : dictionary) {
+                out << pair.first << " - " << pair.second << '\n';
+            }
+            std::cout << "\033[32m" << "The dictionary was successfilly saved in " << filename << "\033[0m" << '\n';
+            out.close();
+        }
+    }
 };
 
-
-// Function to display definition of a term
-void lookupTerm(const std::string& term, const std::map<std::string, std::string>& dictionary) {
-    auto it = dictionary.find(term);
-    if (it != dictionary.end()) {
-        std::cout << it->second << '\n';
-    } else {
-        std::cerr << "There is no such word in the dictionary." << '\n';
-    }
-}
-
-// Function to add a new term-definition pair to the dictionary
-void addTerm(std::string word, std::string definition, std::map<std::string, std::string>& dictionary) {
-    
-}
-
-// Function to delete a term from the dictionary
-void deleteTerm(const std::string& term, std::map<std::string, std::string>& dictionary) {
-    
-}
-
-// Function to save dictionary to a file
-void saveDictionary(const std::string& filename, const std::map<std::string, std::string>& dictionary) {
-    
-}
-
 int main() {
-    std::string path = "/Users/mayakorablina/Yandex.Disk.localized/CodingProjects/cpp_course_2_sem/2023_2024_2nd_sem/week8/15-16_sem/task04/dictionary.txt";
-    std::map<std::string, std::string> dictionary;
-    loadDictionary(path, dictionary);
-    std::string a;
-    std::cout << "Enter a word: ";
-    std::cin >> a;
-    lookupTerm(a, dictionary);
-//    for (const auto& pair : dictionary) {
-//        std::cout << "Ключ: " << pair.first << ", Значение: " << pair.second << std::endl;
-//    }
+    std::string filename = "/Users/mayakorablina/Yandex.Disk.localized/CodingProjects/cpp_course_2_sem/2023_2024_2nd_sem/week8/15-16_sem/task04/dictionary.txt";
+    std::string out = "/Users/mayakorablina/Yandex.Disk.localized/CodingProjects/cpp_course_2_sem/2023_2024_2nd_sem/week8/15-16_sem/task04/new_dictionary.txt";
+    Dictionary dictionary(filename);
+    int choice;
+    std::string term;
+    do {
+        std::cout << "\nDictionary Application\n"
+                  << "1. Look up a term\n"
+                  << "2. Add a new term\n"
+                  << "3. Delete a term\n"
+                  << "4. Save dictionary to file\n"
+                  << "5. Exit\n"
+                  << "Enter your choice: ";
+        std::cin >> choice;
 
-//    std::map<std::string, std::string> dictionary;
-//    std::string filename = "dictionary.txt";
-//    loadDictionary(filename, dictionary);
-//
-//    int choice;
-//    std::string term;
-//
-//    do {
-//        std::cout << "\nDictionary Application\n"
-//                  << "1. Look up a term\n"
-//                  << "2. Add a new term\n"
-//                  << "3. Delete a term\n"
-//                  << "4. Save dictionary to file\n"
-//                  << "5. Exit\n"
-//                  << "Enter your choice: ";
-//        std::cin >> choice;
-//
-//        switch (choice) {
-//            case 1:
-//                std::cout << "Enter the term to look up: ";
-//                std::cin >> term;
-//                lookupTerm(term, dictionary);
-//                break;
-//            case 2:
-//                addTerm(dictionary);
-//                break;
-//            case 3:
-//                std::cout << "Enter the term to delete: ";
-//                std::cin >> term;
-//                deleteTerm(term, dictionary);
-//                break;
-//            case 4:
-//                saveDictionary(filename, dictionary);
-//                break;
-//            case 5:
-//                std::cout << "Exiting the program." << std::endl;
-//                break;
-//            default:
-//                std::cout << "Invalid choice. Please enter a number between 1 and 5." << std::endl;
-//        }
-//    } while (choice != 5);
+        switch (choice) {
+            case 1:
+                std::cout << "Enter the term to look up: ";
+                std::cin >> term;
+                dictionary.lookupTerm(term);
+                break;
+            case 2:
+                std::cout << "Enter a new term: ";
+                std::cin >> term;
+                dictionary.addTerm(term);
+                break;
+            case 3:
+                std::cout << "Enter the term to delete: ";
+                std::cin >> term;
+                dictionary.deleteTerm(term);
+                break;
+            case 4:
+                dictionary.saveDictionary(out);
+                break;
+            case 5:
+                std::cout << "Exiting the program." << std::endl;
+                break;
+            default:
+                std::cout << "Invalid choice. Please enter a number between 1 and 5." << std::endl;
+        }
+    } while (choice != 5);
 
     return 0;
 }
